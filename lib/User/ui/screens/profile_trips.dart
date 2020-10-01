1774f57ebf4716b6a8ab274b138a44f3680bc917
RogerCHDK <rogelio_perez_98@hotmail.com>
2020-09-30 21:43:50 -0500

    Primer version Perfil

--- lib/User/ui/screens/profile_trips.dart
+++ lib/User/ui/screens/profile_trips.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
+import 'profile_header.dart';
+import 'package:programacion_avanzada/User/ui/widgets/profile_places_list.dart';
+import 'package:programacion_avanzada/User/ui/widgets/profile_background.dart';
 
 class ProfileTrips extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    //throw UnimplementedError();
-    return Container(
+    /*return Container(
       color: Colors.indigo,
+    );*/
+    return Stack(
+      children: <Widget>[
+        ProfileBackground(),
+        ListView(
+          children: <Widget>[ProfileHeader(), ProfilePlacesList()],
+        ),
+      ],
     );
   }
 }
